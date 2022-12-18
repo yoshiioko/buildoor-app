ded52cda2c585d978872b0e29132f26f7af453fa
yoshiioko <acardoza@pdx.edu>
2022-12-17 23:07:48 -0800

    setup Chakra UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+
+const colors = {
+  background: "#1F1F1F",
+  accent: "#833BBE",
+  bodyText: "rgba(255, 255, 255, 0.75",
+};
+
+const theme = extendTheme({ colors });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
