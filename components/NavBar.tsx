f0e28766e869242449d0f9d0cfe7d2267369972f
yoshiioko <acardoza@pdx.edu>
2022-12-17 23:39:37 -0800

    Implement the navbar

---
+++ components/NavBar.tsx
@@ -0,0 +1,21 @@
+import { HStack, Spacer } from "@chakra-ui/react";
+import { FC } from "react";
+import styles from "../styles/Home.module.css";
+import dynamic from "next/dynamic";
+
+const WalletMultiButtonDynamic = dynamic(
+  async () =>
+    (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
+  { ssr: false }
+);
+
+const NavBar: FC = () => {
+  return (
+    <HStack width="full" padding={4}>
+      <Spacer />
+      <WalletMultiButtonDynamic
+        className={styles["wallet-adapter-button-trigger"]}
+      />
+    </HStack>
+  );
+};
