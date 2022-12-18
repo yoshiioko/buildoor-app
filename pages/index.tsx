c07cca87f15d41dc32c87660e2debe5636ae1c71
yoshiioko <acardoza@pdx.edu>
2022-12-18 08:40:38 -0800

    update body to render Connected vs Disconnected sites

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,12 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import NavBar from "../components/NavBar";
 import Disconnected from "../components/Disconnected";
+import { useWallet } from "@solana/wallet-adapter-react";
+import Connected from "../components/Connected";
 
 const Home: NextPage = () => {
+  const { connected } = useWallet();
+
   return (
     <div className={styles.container}>
       <Head>
@@ -26,7 +30,7 @@ const Home: NextPage = () => {
           <Spacer />
           <Center>
             {/* If connected, the second view, otherwise the first */}
-            <Disconnected />
+            {connected ? <Connected /> : <Disconnected />}
           </Center>
           <Spacer />
 
