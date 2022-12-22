68cc29ff0bcafcd884b63a5a6d23c6c418efe8f5
yoshiioko <acardoza@pdx.edu>
2022-12-21 17:34:07 -0800

    implement createBldToken() to create a token w/ metadata

--- tokens/bld/assets/index.ts
+++ tokens/bld/assets/index.ts
@@ -1,6 +1,101 @@
 import * as web3 from "@solana/web3.js";
 import * as token from "@solana/spl-token";
 import { initializeKeypair } from "./initializeKeypair";
+import * as fs from "fs";
+import {
+  bundlrStorage,
+  keypairIdentity,
+  Metaplex,
+  toMetaplexFile,
+} from "@metaplex-foundation/js";
+import {
+  DataV2,
+  createCreateMetadataAccountV2Instruction,
+  Data,
+} from "@metaplex-foundation/mpl-token-metadata";
+
+const TOKEN_NAME = "BUILD";
+const TOKEN_SYMBOL = "BLD";
+const TOKEN_DESCRIPTION = "A token for buildoors";
+const TOKEN_IMAGE_NAME = "unicorn.png";
+const TOKEN_IMAGE_PATH = `tokens/bld/assets/${TOKEN_IMAGE_NAME}`;
+
+async function createBldToken(
+  connection: web3.Connection,
+  payer: web3.Keypair
+) {
+  // 1. Create a token with all necessary inputs
+  const tokenMint = await token.createMint(
+    connection, // Connection
+    payer, // Payer
+    payer.publicKey, // Mint Authority
+    payer.publicKey, // Freeze Authority
+    2 // Decimals
+  );
+
+  // 2. Create a metaplex object so we can create metaplex metadata
+  const metaplex = Metaplex.make(connection)
+    .use(keypairIdentity(payer))
+    .use(
+      bundlrStorage({
+        address: "https://devnet.bundlr.network",
+        providerUrl: "https://api.devnet.solana.com",
+        timeout: 60000,
+      })
+    );
+
+  // 3. Read image file
+  const imageBuffer = fs.readFileSync(TOKEN_IMAGE_PATH);
+  const file = toMetaplexFile(imageBuffer, TOKEN_IMAGE_NAME);
+  const imageUri = await metaplex.storage().upload(file);
+
+  // 4. Upload the rest of offchain metadata
+  const { uri } = await metaplex.nfts().uploadMetadata({
+    name: TOKEN_NAME,
+    description: TOKEN_DESCRIPTION,
+    image: imageUri,
+  });
+
+  // 5. Finding out the address where the metadata is stored
+  const metadataPda = metaplex.nfts().pdas().metadata({ mint: tokenMint });
+  const tokenMetadata = {
+    name: TOKEN_NAME,
+    symbol: TOKEN_SYMBOL,
+    uri: uri,
+    sellerFeeBasisPoints: 0,
+    creators: null,
+    collection: null,
+    uses: null,
+  } as DataV2;
+
+  // 6. Create V2 instruction
+  const instruction = createCreateMetadataAccountV2Instruction(
+    {
+      metadata: metadataPda,
+      mint: tokenMint,
+      mintAuthority: payer.publicKey,
+      payer: payer.publicKey,
+      updateAuthority: payer.publicKey,
+    },
+    {
+      createMetadataAccountArgsV2: {
+        data: tokenMetadata,
+        isMutable: true,
+      },
+    }
+  );
+
+  // 7. Create Transaction and add Instruction
+  const transaction = new web3.Transaction();
+  transaction.add(instruction);
+
+  // 8. Send and Confirm Transaction
+  const transactionSignature = await web3.sendAndConfirmTransaction(
+    connection,
+    transaction,
+    [payer]
+  );
+}
 
 async function main() {
   const connection = new web3.Connection(web3.clusterApiUrl("devnet"));
