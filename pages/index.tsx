986089a18156673e2ad17f889c5f3859a60e1841
yoshiioko <acardoza@pdx.edu>
2022-12-25 23:17:14 -0800

    update pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,53 +1,14 @@
-import { Box, Center, Spacer, Stack } from "@chakra-ui/react";
 import type { NextPage } from "next";
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import NavBar from "../components/NavBar";
 import Disconnected from "../components/Disconnected";
-import { useWallet } from "@solana/wallet-adapter-react";
 import Connected from "../components/Connected";
+import { useWallet } from "@solana/wallet-adapter-react";
+import MainLayout from "../components/MainLayout";
 
 const Home: NextPage = () => {
   const { connected } = useWallet();
 
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Buildoors App</title>
-        <meta name="The NFT Collection for Buildoors" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <Box
-        w="full"
-        h="calc(100vh)"
-        bgImage={"url(/home-background.svg)"}
-        backgroundPosition="center"
-      >
-        <Stack w="full" h="calc(100vh)" justify="center">
-          {/* NavBar */}
-          <NavBar />
-          <Spacer />
-          <Center>
-            {/* If connected, the second view, otherwise the first */}
-            {connected ? <Connected /> : <Disconnected />}
-          </Center>
-          <Spacer />
-
-          <Center>
-            <Box marginBottom={4} color="white">
-              <a
-                href="https://twitter.com/adrianisawank"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                built with @_buildspace
-              </a>
-            </Box>
-          </Center>
-        </Stack>
-      </Box>
-    </div>
+    <MainLayout> {connected ? <Connected /> : <Disconnected />}</MainLayout>
   );
 };
 
